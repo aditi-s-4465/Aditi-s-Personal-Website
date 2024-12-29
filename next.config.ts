78005f82ec08b926aa0ff67229a6b4a461fbecb9
aditi-s-4465 <aditi12@illinois.edu>
2024-12-29 12:37:25 -0800

    fixing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  useFileSystemPublicRoutes: true,
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
   },
-  assetPrefix: isProd ? '/Aditi-s-Personal-Website' : '',
-  basePath: isProd ? '/Aditi-s-Personal-Website' : '',
-  output: 'export'
-  
+  output: 'export',
 };
+
 
 export default nextConfig;
